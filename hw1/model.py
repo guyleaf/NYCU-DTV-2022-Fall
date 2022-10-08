23f40e261b8ef1d62e0ae410c6eab14e7f3912ef
guyleaf <leaf.ying.work@gmail.com>
2022-10-09 07:29:36 +0800

    Save VGG19 network

--- hw1/model.py
+++ hw1/model.py
@@ -1,7 +1,43 @@
-﻿import torch
+﻿from typing import Tuple, Union
+import torch
 import torch.nn as nn
 
-from utils import make_convs
+
+def make_convs(
+    num_layers: int,
+    kernel_size: Union[int, Tuple[int, int]],
+    in_channels: int,
+    out_channels: int,
+    stride: Union[int, Tuple[int, int]] = 1,
+    padding: Union[int, Tuple[int, int]] = 1,
+    bias: bool = True,
+    activation: str = "relu",
+) -> list[torch.nn.Module]:
+    if activation == "relu":
+        activation_fn = torch.nn.ReLU(inplace=True)
+    elif activation == "leakyRelu":
+        activation_fn = torch.nn.LeakyReLU(0.2, inplace=True)
+    else:
+        raise RuntimeError(
+            f"The activation function {activation} is not implemented."
+        )
+
+    layers = []
+    for _ in range(num_layers):
+        layers.append(
+            torch.nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride=stride,
+                padding=padding,
+                bias=bias,
+            )
+        )
+        layers.append(torch.nn.BatchNorm2d(out_channels))
+        layers.append(activation_fn)
+        in_channels = out_channels
+    return layers
 
 
 class Network(nn.Module):
@@ -75,12 +111,6 @@ class Network(nn.Module):
             # 7 * 7 * 512
             nn.Flatten(),
             nn.Linear(in_features=7 * 7 * 512, out_features=out_channels),
-            # nn.ReLU(inplace=True),
-            # nn.Dropout(p=0.5),
-            # nn.Linear(in_features=4096, out_features=4096),
-            # nn.ReLU(inplace=True),
-            # nn.Dropout(p=0.5),
-            # nn.Linear(in_features=4096, out_features=out_channels),
         )
 
     @property
