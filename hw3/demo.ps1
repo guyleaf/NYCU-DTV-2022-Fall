b15692d96e11cf98f98df25252b5b527216b0dc2
guyleaf <leaf.ying.work@gmail.com>
2022-12-01 14:46:45 +0800

    Add comment to track.yaml and Update demo.ps1 script

--- hw3/demo.ps1
+++ hw3/demo.ps1
@@ -1,4 +1,28 @@
 ﻿$dataDir = "data/demo/images"
+# $dataDir = "data"
+$datasetName = "DEMO"
+# $datasetName = "MOT20-ALL"
+$framerate = 30
 
-python src/track.py with dataset_name=DEMO data_root_dir=$dataDir output_dir=results/demo_deformable write_images=pretty
-# python src/track.py with dataset_name=DEMO data_root_dir=$dataDir output_dir=results/demo write_images=pretty obj_detect_checkpoint_file=models/mots20_train_masks/checkpoint.pth generate_attention_maps=True
+$suffix = "mot20_crowdhuman_deformable_multi_frame"
+$model = "models/$suffix/checkpoint_epoch_50.pth"
+
+$baseOutputDir = "F:/NYCU/DTV/hw3"
+
+for ($i = 5; $i -le 50; $i += 5)
+{
+    $fileName = "demo_reid_${suffix}_${i}" 
+    $outputDir = "$baseOutputDir/$fileName"
+
+    python src/track.py with reid `
+        obj_detect_checkpoint_file=$model `
+        dataset_name=$datasetName `
+        data_root_dir=$dataDir `
+        output_dir=$outputDir `
+        write_images=pretty `
+        tracker_cfg.inactive_patience=$i
+
+    wsl ffmpeg -framerate $framerate `
+        -i "$outputDir/**/%06d.png" `
+        -vcodec "libx264" "$baseOutputDir/videos/$fileName.mp4"
+}
