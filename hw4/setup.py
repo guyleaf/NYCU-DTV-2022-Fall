1d6bc5d6b787b70149836f97c536ab353d1fc229
guyleaf <leaf.ying.work@gmail.com>
2023-01-06 19:59:00 +0800

    Add openvino version of yolov7 & course material

--- hw4/setup.py
+++ hw4/setup.py
@@ -5,6 +5,8 @@ setup(
     packages=["live_streaming_server"],
     version="0.0.1",
     install_requires=[
+        "openvino==2022.3.0",
+        "numpy",
         "typed-argument-parser~=1.7.2",
         "Flask~=2.2.2",
         "python-ffmpeg-video-streaming~=0.1.15",
